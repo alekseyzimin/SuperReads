c8f9bb11f30b3f2e7171b3a8622f9ee307059b15
Aleksey Zimin <alekseyz@genome6.umd.edu>
2011-09-21 17:10:36 -0400

    Minor bug fix

--- src/recompute_astat_superreads.sh
+++ src/recompute_astat_superreads.sh
@@ -5,7 +5,7 @@ READLEN=$3;
 READ_SR=$4;
 gatekeeper -dumpfragments -tabular ${ASM_DIR}/${PREFIX}.gkpStore |awk '{print $1}' > ${PREFIX}.uid
 tigStore -g ${ASM_DIR}/${PREFIX}.gkpStore -t ${ASM_DIR}/${PREFIX}.tigStore 2 -U -d layout > unitig_layout.txt
-cat unitig_layout.txt | ${SHARED_PATH}compute_sr_cov.pl ${PREFIX}.uid $READ_SR $READLEN 1> unitig_cov.txt 2>global_arrival_rate.txt
+cat unitig_layout.txt | compute_sr_cov.pl ${PREFIX}.uid $READ_SR $READLEN 1> unitig_cov.txt 2>global_arrival_rate.txt
 tigStore -g ${ASM_DIR}/${PREFIX}.gkpStore -t ${ASM_DIR}/${PREFIX}.tigStore 2 -E unitig_cov.txt 1> tigStore.err 2>&1
 tigStore -g ${ASM_DIR}/${PREFIX}.gkpStore -t ${ASM_DIR}/${PREFIX}.tigStore 1 -E unitig_cov.txt 1> tigStore.err 2>&1
 
