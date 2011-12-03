35c702730526dea4713e068e3d68b49fc47d2a0c
Aleksey Zimin <alekseyz@genome7.umd.edu>
2011-12-03 17:11:22 -0500

    chenged main pipeline -- it is not a good idea to merge before excluding single-count mate pairs

---
+++ src/extractreads_not.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+$infile1 = $ARGV[0]; #file with read names
+$infile2 = $ARGV[1]; #fasta file
+$fieldnum = $ARGV[2]-1; #field number
+
+open (FILE1, $infile1);
+open (FILE2, $infile2);
+my %readnames;
+while ($line = <FILE1>)
+  {
+    chomp($line);
+    $readnames{$line}=1;
+  }
+close(FILE1);
+
+my $sequence="";
+my $readname="";
+open (OUTFILE, ">$outfile");
+while ($line = <FILE2>)
+{
+    if ($line =~ /^>/)
+    {
+	if(not(defined $readnames{$readname}))
+	{
+	print ">$readname\n$sequence\n";
+	}
+	chomp($line);
+        @f=split(/\s+/,substr($line,1));
+        $readname=$f[$fieldnum];
+        $sequence="";
+    }
+    else
+    {
+     chomp($line);
+     $sequence.=$line;
+    }
+}
+        if(defined $readnames{$readname})
+        {
+        print ">$readname\n$sequence\n";
+        }
+
+close (FILE2);
+
