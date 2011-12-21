1bfe2af9e5b97e269ff175cc17d506a88f6b9360
Guillaume Marçais <gmarcais@umd.edu>
2011-12-20 22:48:58 -0500

    Cleanup and speed up getSuperReadInsertCountsFromReadPlacementFile.cc

--- src2/getSuperReadInsertCountsFromReadPlacementFile.cc
+++ src2/getSuperReadInsertCountsFromReadPlacementFile.cc
@@ -11,62 +11,58 @@
 
 using namespace std;
 
+struct str_comp {
+  bool operator()(const char *s1, const char *s2) {
+    return strcmp(s1, s2) < 0;
+  }
+};
+
 int main (int argc, char **argv)
 {
-     map<string,int> superReadToCounts;
-     map<string,int>::iterator it;
-     string str;
-     int isFirstLine, numFlds;
-     charb line, line2, *cptr;
-     char *prefixHold;
-     long long readNumHold, readNum;
-     char *superReadHold;
-     ExpBuffer<char *> flds;
+  typedef map<const char *, int, str_comp> read_count_map;
+  read_count_map    superReadToCounts;
+  charb*            lines = new charb[2];
+  charb*            cptr;
+  int               line  = 1;
+  char*             prefixHold;
+  long long         readNumHold, readNum;
+  char*             superReadHold;
+  ExpBuffer<char *> flds;
 
-     isFirstLine = 1;
-     cptr = &line;
-     if(!fgets (*cptr, stdin))
-       die << "Invalid input: empty file";
-     numFlds = getFldsFromLine (*cptr, flds);
-     str = string (flds[1]);
-     superReadToCounts.insert(superReadToCounts.begin(), pair<string,int>(str, 1));
-     superReadHold = flds[1];
-     prefixHold = flds[0]; // Just use the first 2 chars
-     readNumHold = atoll (*cptr+2);
-     cptr = &line2;
-     while (fgets (*cptr, stdin)) {
-       numFlds = getFldsFromLine (*cptr, flds);
-	  readNum = atoll(*cptr+2);
-	  // The next excludes counting the second read of a mate pair from
-	  // the counts for the super-read. We want super-reads which have
-	  // reads coming from at least 2 different inserts. Our read counts
-	  // for the inserts will (probably) be off.
+  cptr = &lines[(line = !line)];
+  if(!fgets (*cptr, stdin))
+    die << "Invalid input: empty file";
+  getFldsFromLine (*cptr, flds);
+  const char *nstr = strdup(flds[1]);
+  ++superReadToCounts[nstr];
+  superReadHold = flds[1];
+  prefixHold    = flds[0];      // Just use the first 2 chars
+  readNumHold   = atoll (*cptr+2);
+  cptr          = &lines[(line = !line)];
+  while (fgets (*cptr, stdin)) {
+    getFldsFromLine (*cptr, flds);
+    readNum = atoll(*cptr+2);
+    // The next excludes counting the second read of a mate pair from
+    // the counts for the super-read. We want super-reads which have
+    // reads coming from at least 2 different inserts. Our read counts
+    // for the inserts will (probably) be off.
 #if 1
-	  if ((strcmp(flds[1], superReadHold) == 0) &&
-	      (readNumHold+1 == readNum) &&
-	      (readNum % 2 == 1) &&
-	      (strncmp(prefixHold, *cptr, 2) == 0))
-	       continue;
+    if ((strcmp(flds[1], superReadHold) == 0) &&
+        (readNumHold+1 == readNum) &&
+        (readNum % 2 == 1) &&
+        (strncmp(prefixHold, *cptr, 2) == 0))
+      continue;
 #endif
-	  str = string (flds[1]);
-	  it = superReadToCounts.find(str);
-	  if (it == superReadToCounts.end()) {
-	       it = superReadToCounts.begin();
-	       superReadToCounts.insert (it, pair<string,int>(str, 1));
-	  }
-	  else {
-	       ++(*it).second;
-	  }
-	  superReadHold = flds[1];
-	  prefixHold = flds[0];
-	  readNumHold = readNum;
-	  if (cptr == &line)
-	       cptr = &line2;
-	  else
-	       cptr = &line;
-     }
-     for (it=superReadToCounts.begin(); it != superReadToCounts.end(); it++)
-       cout << (*it).second << " " << (*it).first << endl;
+    nstr = strdup(flds[1]);
+    ++superReadToCounts[nstr];
+    superReadHold = flds[1];
+    prefixHold    = flds[0];
+    readNumHold   = readNum;
+    cptr          = &lines[(line = !line)];
+  }
+  for (read_count_map::const_iterator it = superReadToCounts.begin(); 
+       it != superReadToCounts.end(); ++it)
+    cout << it->second << " " << it->first << "\n";
 
-     return (0);
+  return (0);
 }
