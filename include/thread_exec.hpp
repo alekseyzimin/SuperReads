0f8333804ca5022375604df589d09ed7d8661db0
Guillaume Marçais <gmarcais@umd.edu>
2012-03-16 14:34:00 -0400

    Some documentation for thread_exec class

--- include/thread_exec.hpp
+++ include/thread_exec.hpp
@@ -25,6 +25,32 @@
 #include <string.h>
 #include <err.hpp>
 
+/** Thread_exec is a helper class to spawn threads. To use, create a
+ * class inheriting from thread_exec and implement the virtual method
+ * start, which is the routine that you want to run in parallel. Upon
+ * calling exec(n), the start method will be called n times, each in a
+ * different thread. The parameter id to start will have value 0 to
+ * n-1.
+ * 
+ * The exec_join method does exec and then join for all the
+ * threads. I.e., unlike exec which returns immediately, exec_join
+ * returns when all the threads are done.
+ *
+ *  Example:
+
+class MyThread : public thread_exec {
+public:
+  virtual void start(int id) {
+    std::cout << "I am thread " << id << " " << pthread_self() << "\n";
+  }
+};
+
+MyThread t;
+t.exec_join(5);
+
+  The start method is called 5 times and 0 to 4 will be printed in some random order with a different thread number.
+*/
+
 class thread_exec {
   struct thread_info {
     int          id;
