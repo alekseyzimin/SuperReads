65ec7d1efc797dcf220cc014e1d415510c025d6d
Guillaume Marçais <gmarcais@umd.edu>
2012-04-10 18:27:50 -0400

    Yaggo headers are now autogenerated.

--- src/error_correct_reads.cc
+++ src/error_correct_reads.cc
@@ -16,22 +16,6 @@
  */
 
 
-/*  This file is part of k_unitig.
-
-    k_unitig is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    k_unitig is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with k_unitig.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <vector>
 #include <memory>
 
