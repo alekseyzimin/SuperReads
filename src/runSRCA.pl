d6f08cfcdfa120b25a2a080183784e10701126ed
Aleksey Zimin <alekseyz@ipst.umd.edu>
2013-02-27 14:41:46 -0500

    switched name of jellyfish in runSRCA

--- src/runSRCA.pl
+++ src/runSRCA.pl
@@ -723,7 +723,7 @@ print FILE "recompute_astat_superreads.sh genome CA \$PE_AVG_READ_LENGTH work1/r
 
 #here we filter for repetitive kmers in the unique unitigs
 print FILE "cd CA\n";
-print FILE "tigStore -g genome.gkpStore -t genome.tigStore 2 -d layout -U | tr -d '-' | awk 'BEGIN{print \">unique unitigs\"}{if(\$1 == \"cns\"){seq=\$2}else if(\$1 == \"data.unitig_coverage_stat\" && \$2>=5){print seq\"N\"}}' | jellyfish2 count -L 2 -C -m $ovlMerSize -s \$ESTIMATED_GENOME_SIZE -t $NUM_THREADS -o unitig_mers /dev/fd/0\n";
+print FILE "tigStore -g genome.gkpStore -t genome.tigStore 2 -d layout -U | tr -d '-' | awk 'BEGIN{print \">unique unitigs\"}{if(\$1 == \"cns\"){seq=\$2}else if(\$1 == \"data.unitig_coverage_stat\" && \$2>=5){print seq\"N\"}}' | jellyfish-2.0 count -L 2 -C -m $ovlMerSize -s \$ESTIMATED_GENOME_SIZE -t $NUM_THREADS -o unitig_mers /dev/fd/0\n";
 print FILE "overlapStore -d genome.ovlStore | filter_overlap_file -t $NUM_THREADS <(gatekeeper -dumpfastaseq genome.gkpStore ) unitig_mers /dev/fd/0 |convertOverlap -b -ovl > overlaps.ovb\n";
 print FILE "rm -rf 4-unitigger 5-consensus genome.tigStore genome.ovlStore\n";
 print FILE "overlapStore -c genome.ovlStore -M 4096 -t $NUM_THREADS -g genome.gkpStore overlaps.ovb 1>overlapstore.err 2>&1\n";
