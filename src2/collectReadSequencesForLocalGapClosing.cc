bafa4a91c8f6b847330f8866743eeaea7d0416a1
Aleksey Zimin <alekseyz@ipst.umd.edu>
2013-10-15 13:44:46 -0400

    readnames are back in readFile.000

--- src2/collectReadSequencesForLocalGapClosing.cc
+++ src2/collectReadSequencesForLocalGapClosing.cc
@@ -272,12 +272,12 @@ int main(int argc, char **argv)
 			 stdString mateRead = getReadMateName (readName);
 //			 outfile << readName << outputReadHdr[readName] << '\n';
 			 if (readType[readName] != 3)
-			      outfile << "r\n" << readSeq[readName] << "\n";
+			      outfile << readName << "\n" << readSeq[readName] << "\n";
 //			 else
 //			      outfile << "N\n";
 //			 outfile << mateRead << outputReadHdr[mateRead] << '\n';
 			 if (readType[mateRead] != 3)
-			      outfile << "m\n" << readSeq[mateRead] << "\n";
+			      outfile << mateRead << "\n" << readSeq[mateRead] << "\n";
 //			 else
 //			      outfile << "N\n";
 		    }
