f23ea6585a279754ae5e5039121172a883951eba
Aleksey Zimin <alekseyz@ipst.umd.edu>
2014-02-04 14:05:15 -0500

    small change to clean up stdout

--- src/recompute_astat_superreads.sh
+++ src/recompute_astat_superreads.sh
@@ -8,7 +8,7 @@ tigStore -g ${ASM_DIR}/${PREFIX}.gkpStore -t ${ASM_DIR}/${PREFIX}.tigStore 2 -U 
 cat unitig_layout.txt | compute_sr_cov.pl ${PREFIX}.uid $READ_SR $READLEN superReadSequences_shr.frg  1> unitig_cov.txt 2>global_arrival_rate.txt
 tigStore -g ${ASM_DIR}/${PREFIX}.gkpStore -t ${ASM_DIR}/${PREFIX}.tigStore 2 -E unitig_cov.txt 1> tigStore.err 2>&1
 tigStore -g ${ASM_DIR}/${PREFIX}.gkpStore -t ${ASM_DIR}/${PREFIX}.tigStore 1 -E unitig_cov.txt 1> tigStore.err 2>&1
-getGCBiasStatistics.perl --assembly-directory . --min-from-end 200 --default-read-length $READLEN
+getGCBiasStatistics.perl --assembly-directory . --min-from-end 200 --default-read-length $READLEN 1>getGCBiasStatistics.err 2>&1
 makeAdjustmentFactorsForNumReadsForAStatBasedOnGC_v2 unitig_layout.txt > unitigCoverageAdjustedForGC.txt
 cat unitig_layout.txt | compute_sr_cov.revisedForGCContig.pl ${PREFIX}.uid $READ_SR $READLEN superReadSequences_shr.frg unitigCoverageAdjustedForGC.txt 1> unitig_cov.GCadjusted.txt 2>global_arrival_rate.GCadjusted.txt
 tigStore -g ${ASM_DIR}/${PREFIX}.gkpStore -t ${ASM_DIR}/${PREFIX}.tigStore 2 -E unitig_cov.GCadjusted.txt 1> tigStore.GCadjusted.err 2>&1
