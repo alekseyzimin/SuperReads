e90c845116b52dd88c01f22ebc4759c020801018
Aleksey Zimin <alekseyz@ipst.umd.edu>
2014-03-16 08:29:43 -0400

    changed condition in recompute astat

--- src/recompute_astat_superreads.sh
+++ src/recompute_astat_superreads.sh
@@ -9,11 +9,12 @@ cat unitig_layout.txt | compute_sr_cov.pl ${PREFIX}.uid $READ_SR $READLEN superR
 tigStore -g ${ASM_DIR}/${PREFIX}.gkpStore -t ${ASM_DIR}/${PREFIX}.tigStore 2 -E unitig_cov.txt 1> tigStore.err 2>&1
 tigStore -g ${ASM_DIR}/${PREFIX}.gkpStore -t ${ASM_DIR}/${PREFIX}.tigStore 1 -E unitig_cov.txt 1> tigStore.err 2>&1
 getGCBiasStatistics.perl --assembly-directory . --min-from-end 200 --default-read-length $READLEN 1>getGCBiasStatistics.err 2>&1
-if [ ! -s adjustmentFactorsForGCPct.txt ]
-    then
-    exit;
-fi
+
+if [ -s adjustmentFactorsForGCPct.txt ]
+then
 makeAdjustmentFactorsForNumReadsForAStatBasedOnGC_v2 unitig_layout.txt > unitigCoverageAdjustedForGC.txt
 cat unitig_layout.txt | compute_sr_cov.revisedForGCContig.pl ${PREFIX}.uid $READ_SR $READLEN superReadSequences_shr.frg unitigCoverageAdjustedForGC.txt 1> unitig_cov.GCadjusted.txt 2>global_arrival_rate.GCadjusted.txt
 tigStore -g ${ASM_DIR}/${PREFIX}.gkpStore -t ${ASM_DIR}/${PREFIX}.tigStore 2 -E unitig_cov.GCadjusted.txt 1> tigStore.GCadjusted.err 2>&1
 tigStore -g ${ASM_DIR}/${PREFIX}.gkpStore -t ${ASM_DIR}/${PREFIX}.tigStore 1 -E unitig_cov.GCadjusted.txt 1> tigStore.GCadjusted.err 2>&1
+fi
+
