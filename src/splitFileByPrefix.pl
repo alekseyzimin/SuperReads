8547ce6bb091bcec844011f370f7da651792fa2b
Aleksey Zimin <alekseyz@ipst.umd.edu>
2015-04-22 14:41:33 -0400

    changes to masurca to run soap: expect the soap config file to be named soap_config

---
+++ src/splitFileByPrefix.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+my $index=0;
+my @fh=();
+my %findex=();
+
+foreach my $v (@ARGV) {
+            local *OUTFILE;
+            open(OUTFILE,">$v.cor.clean.fa");
+            push(@fh,*OUTFILE);
+            $findex{$v}=$index;
+            ++$index;
+}
+
+my $current_index=0;
+while($line=<STDIN>){
+$current_index=$findex{substr($line,1,2)} if($line=~ /^>/);
+print $fh[$current_index] $line;
+$line=<STDIN>;
+print $fh[$current_index] $line;
+}
+foreach my $v (@fh) {
+close($v);
+}
+
